453dc347ca97440c4d63876f78c030c012570bfe
Xiaolai Ren <1942769064@qq.com>
2026-07-15 11:34:37 +0800

    git commit -m "Day3 python practice"

--- python/practice.py
+++ python/practice.py
@@ -1,3 +1,4 @@
+import random
 #计算1-100的和
 total=0
 for i in range(1,101):
@@ -22,3 +23,31 @@ sum_Tom=student['english']+student["math"]+student['python']
 print(f"总分为：{sum_Tom}")
 avg_Tom=sum_Tom/3
 print(f"平均分为：{avg_Tom}")
+#猜数字游戏
+num=random.randint(1,100)
+while True:
+    num1=int(input("请输入一个数:"))
+    if num==num1:
+        print("猜对了！")
+        break
+    else:
+        print("猜错了！")
+#成绩等级
+score=int(input("请输入成绩："))
+if score>=90 and score<=100:
+    print("优秀")
+elif score>=80 and score<90:
+    print("良好")
+elif score>=70 and score<80:
+    print("中等")
+elif score>=60 and score<70:
+    print("及格")
+elif score<60:
+    print("不及格")
+#函数练习
+def average(scores):
+    total=0
+    for score in scores:
+        total+=score
+    #或简洁写法total=sum(scores)
+    return total/len(scores)
